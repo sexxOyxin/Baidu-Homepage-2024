832fb36441be1dd4c611f1a97f65b1794aa8eee8
sexxOyxin <90915214+sexxOyxin@users.noreply.github.com>
2024-08-29 15:18:11 +0800

    2024-08-29_pyupdate

--- assets/scripts/data.py
+++ assets/scripts/data.py
@@ -1,7 +1,3 @@
-import csv  #用于把爬取的数据存储为csv格式，可以excel直接打开的
-import time  #用于对请求加延时，爬取速度太快容易被反爬
-from time import sleep #同上
-import random  #用于对延时设置随机数，尽量模拟人的行为
 import requests  #用于向网站发送请求
 from lxml import etree    #lxml为第三方网页解析库，强大且速度快
 
@@ -10,7 +6,23 @@ headers = {
     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
 }
 response = requests.get(url, headers=headers, timeout=10)
+
 html = response.text
+
 parse = etree.HTML(html)  #解析网页
-all_tr = parse.xpath('//*[@id="hotsearch_data"]')
-print(all_tr)
+# 获取所有 li 元素
+items = parse.xpath('//*[@id="hotsearch-content-wrapper"]/li')
+
+# 初始化结果列表
+data = []
+
+# 遍历每个 li，提取标题和链接
+for item in items:
+    title = item.xpath('.//a//span[@class="title-content-title"]/text()')[0]  # 获取标题
+    link = item.xpath('.//a/@href')[0]  # 获取链接
+
+    # 将标题和链接存入字典并添加到结果列表中
+    data.append({"title": title, "link": link})
+
+# 打印结果
+print(data)
